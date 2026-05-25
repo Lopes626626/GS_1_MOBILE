680496e35cdd939c4cfbad02141447e8f822e49f
Rafael <benedetti.rafael626@gmail.com>
2026-05-25 10:04:24 -0300

    chore: estrutura inicial gerada pelo Spring Initializr

---
+++ Backend/missaoespacial/src/main/java/br/com/fiap/missaoespacial/MissaoespacialApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.missaoespacial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissaoespacialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissaoespacialApplication.class, args);
+	}
+
+}
---
+++ Backend/missaoespacial/src/test/java/br/com/fiap/missaoespacial/MissaoespacialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.missaoespacial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissaoespacialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
